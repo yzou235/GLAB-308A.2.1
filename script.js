612fde0ec8e586bc1552517b6293fdd9762d8fbe
yzou235 <yumingzou235@gmail.com>
2023-12-28 22:00:25 -0500

    Added JSDoc comments to describe the purpose of the 'roll' method.

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ const adventurer = {
             type: "Flea",
             belongings: ["hat", "sunglasses"]
         }
-    }
+    },
+    /**
+     * Rolls a 20-sided die and logs the result.
+     * @param {number} mod - the modifier to be added to the roll result.
+     */
     roll (mod = 0) {
         const result = Math.floor(Math.random()*20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
